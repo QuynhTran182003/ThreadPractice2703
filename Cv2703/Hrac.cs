4956d83b626e51ae0a7f39fa4d912c482743202d
Tran Huong Quynh <tran2@SPSEJECNANET.CZ>
2023-03-27 08:26:01 +0200

    first commit

---
+++ Cv2703/Hrac.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cv2703
+{
+    public class Hrac
+    {
+        private string name;
+        private int health;
+        private bool isLiving;
+        private static readonly Object locker = new Object();
+
+        public string Name {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Health
+        {
+            get { return this.health; }
+            set { this.health = value; }
+        }
+        public bool IsLiving
+        {
+            get { return Health > 0 ? true : false; }
+            private set { this.isLiving = value; }
+        }
+
+        public Hrac(string name, int health)
+        {
+            Name = name;
+            Health = health;
+        }
+
+        public void DecreaseHealth(int hodnota)
+        {
+            lock (locker)
+            {
+                if (IsLiving)
+                {
+                    Health -= hodnota;
+
+                    Console.WriteLine($"Decreasing {hodnota} ... Remains: {Health}");
+                }
+            }
+            
+        }
+
+        public void IncreaseHealth(int hodnota)
+        {
+            lock (locker)
+            {
+                if (IsLiving)
+                {
+                    Health += hodnota;
+                    Console.WriteLine($"increasing {hodnota} ...Remains: {Health}");
+                }
+            }
+        }
+    }
+
+
+}
---
+++ Cv2703/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Cv2703
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hrac h = new Hrac("Krixi", 500);
+            Thread thread1 = new Thread(new ThreadStart(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    h.DecreaseHealth(new Random().Next(500)+100);
+                }
+            }
+            ));
+
+            Thread thread2 = new Thread(new ThreadStart(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    h.IncreaseHealth(new Random().Next(50));
+                }
+            }
+            ));
+            thread1.Start();
+            thread2.Start();
+        }
+    }
+}
